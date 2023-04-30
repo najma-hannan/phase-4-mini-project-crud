cf8d39e72a4f9bd2b784b44b3213ccab7b1cd12b
Hannan <najma.mahat@student.moringaschool.com>
2023-04-30 11:58:08 +0300

    Completed crud and fixed all the errors

--- db/migrate/20230430083601_create_spices.rb
+++ db/migrate/20230430083601_create_spices.rb
@@ -1,6 +1,11 @@
 class CreateSpices < ActiveRecord::Migration[6.1]
   def change
     create_table :spices do |t|
+      t.string :title
+      t.string :image
+      t.string :description
+      t.string :notes
+      t.float :rating
 
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,13 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema.define(version: 2023_04_30_083601) do
-
   create_table "spices", force: :cascade do |t|
+    t.string "title"
+    t.string "image"
+    t.string "description"
+    t.string "notes"
+    t.float "rating"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
-
 end
